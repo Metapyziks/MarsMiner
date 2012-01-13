7ecfeae736142a23c1467a7cbd3749afc2c2b7c8
Metapyziks <metapyziks@gmail.com>
2012-01-13 14:43:52 +0000

    Moved Octant and OctreeEnumerator into their own files.

---
+++ MarsMiner.Shared/Octant.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarsMiner.Shared
+{
+    public struct Octant
+    {
+        public static readonly Octant[] All = new Octant[]
+        {
+            new Octant( 0, 0, 0 ),
+            new Octant( 0, 0, 1 ),
+            new Octant( 0, 1, 0 ),
+            new Octant( 0, 1, 1 ),
+            new Octant( 1, 0, 0 ),
+            new Octant( 1, 0, 1 ),
+            new Octant( 1, 1, 0 ),
+            new Octant( 1, 1, 1 ),
+        };
+
+        public static readonly Octant[,,] XYZ = new Octant[ , , ]
+        {
+            {
+                {
+                    All[ 0 ],
+                    All[ 1 ],
+                },
+                {
+                    All[ 2 ],
+                    All[ 3 ],
+                }
+            },
+            {
+                {
+                    All[ 4 ],
+                    All[ 5 ],
+                },
+                {
+                    All[ 6 ],
+                    All[ 7 ],
+                }
+            }
+        };
+
+        public readonly int X;
+        public readonly int Y;
+        public readonly int Z;
+
+        public readonly int Index;
+
+        public Octant Next
+        {
+            get { return All[ ( Index + 1 ) % 8 ]; }
+        }
+
+        private Octant( int x, int y, int z )
+        {
+            X = x;
+            Y = y;
+            Z = z;
+
+            Index = x << 2 | y << 1 | z;
+        }
+
+        public override bool Equals( object obj )
+        {
+            return obj is Octant && ( (Octant) obj ).Index == Index;
+        }
+
+        public override int GetHashCode()
+        {
+            return Index;
+        }
+    }
+}
---
+++ MarsMiner.Shared/OctreeEnumerator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarsMiner.Shared
+{
+    public class OctreeEnumerator<T> : IEnumerator<Octree<T>>
+    {
+        private OctreeEnumerator<T> myChild;
+        private Octant myCurOctant;
+        private bool myFirst;
+
+        public readonly Octree<T> Octree;
+
+        public OctreeEnumerator( Octree<T> octree )
+        {
+            Octree = octree;
+
+            myChild = null;
+            myCurOctant = Octant.All[ 0 ];
+            myFirst = true;
+        }
+
+        public Octree<T> Current
+        {
+            get
+            {
+                if ( Octree.HasChildren )
+                    return myChild.Current;
+
+                return Octree;
+            }
+        }
+
+        object System.Collections.IEnumerator.Current
+        {
+            get { return Current; }
+        }
+
+        public bool MoveNext()
+        {
+            if ( Octree.HasChildren )
+            {
+                if ( myChild == null || !myChild.MoveNext() )
+                {
+                    if ( myChild != null && myCurOctant.Index == 0 )
+                        return false;
+
+                    myChild = new OctreeEnumerator<T>( Octree[ myCurOctant ] );
+                    myChild.MoveNext();
+                    myCurOctant = myCurOctant.Next;
+                }
+
+                return true;
+            }
+
+            myFirst = !myFirst;
+            return !myFirst;
+        }
+
+        public void Reset()
+        {
+            myChild = null;
+            myCurOctant = Octant.All[ 0 ];
+        }
+
+        public void Dispose()
+        {
+            if ( myChild != null )
+                myChild.Dispose();
+        }
+    }
+}
