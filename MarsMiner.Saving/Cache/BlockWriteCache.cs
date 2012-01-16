25c3f514b4bf7247281658ddbf83d03b5c2ddbaa
Tamschi <MailTamschi@googlemail.com>
2012-01-16 12:42:02 +0100

    Started working on saving
    --HG--
    branch : saving

---
+++ MarsMiner.Saving/Cache/BlockWriteCache.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MarsMiner.Saving.Interfaces;
+
+#if AssertCorrectSaving
+using System.Diagnostics;
+#endif
+
+namespace MarsMiner.Saving.Cache
+{
+    internal class BlockWriteCache
+    {
+        public byte[] Data { get; private set; }
+
+#if AssertCorrectSaving
+        List<int> blanks;
+#endif
+
+        //TODO: public event Action<Pointer
+
+        public BlockWriteCache(byte[] data, IEnumerable<KeyValuePair<int, IBlockStructure>> pointers)
+        {
+
+        }
+
+        private void FillBlank(int offset, byte[] data)
+        {
+#if AssertCorrectSaving
+            Debug.Assert(blanks.Contains(offset));
+#endif
+            data.CopyTo(Data, offset);
+#if AssertCorrectSaving
+            blanks.Remove(offset);
+#endif
+        }
+    }
+}
