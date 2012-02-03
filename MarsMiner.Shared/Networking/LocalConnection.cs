d27aa79a4a2350ba78a04def8b4238f4fedd1a71
Metapyziks <metapyziks@gmail.com>
2012-02-03 18:44:31 +0000

    Added networking fundamentals

---
+++ MarsMiner.Shared/Networking/LocalConnection.cs
@@ -0,0 +1,136 @@
+﻿/**
+ * Copyright (c) 2012 James King [metapyziks@gmail.com]
+ *
+ * This file is part of MarsMiner.
+ * 
+ * MarsMiner is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * MarsMiner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MarsMiner. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.IO;
+using System.Threading;
+
+namespace MarsMiner.Shared.Networking
+{
+    public static class LocalConnection
+    {
+        private static int myClientToServerWaitingPackets = 0;
+        private static MemoryStream myClientToServerStream = new MemoryStream();
+        private static long myLastClientToServerReadPos = 0;
+        private static long myLastClientToServerWritePos = 0;
+
+        private static int myServerToClientWaitingPackets = 0;
+        private static MemoryStream myServerToClientStream = new MemoryStream();
+        private static long myLastServerToClientReadPos = 0;
+        private static long myLastServerToClientWritePos = 0;
+
+        public static Stream StartClientToServerPacket()
+        {
+            Monitor.Enter( myClientToServerStream );
+            myClientToServerStream.Position = myLastClientToServerWritePos;
+            return myClientToServerStream;
+        }
+
+        public static Stream StartServerToClientPacket()
+        {
+            Monitor.Enter( myServerToClientStream );
+            myServerToClientStream.Position = myLastServerToClientWritePos;
+            return myServerToClientStream;
+        }
+
+        public static void SendClientToServerPacket()
+        {
+            myLastClientToServerWritePos = myClientToServerStream.Position;
+            Monitor.Exit( myClientToServerStream );
+            ++myClientToServerWaitingPackets;
+        }
+
+        public static void SendServerToClientPacket()
+        {
+            myLastServerToClientWritePos = myServerToClientStream.Position;
+            Monitor.Exit( myServerToClientStream );
+            ++myServerToClientWaitingPackets;
+        }
+
+        public static bool ClientToServerPending()
+        {
+            return myClientToServerWaitingPackets != 0;
+        }
+
+        public static bool ServerToClientPending()
+        {
+            return myServerToClientWaitingPackets != 0;
+        }
+
+        public static BinaryReader ReadClientToServerPacket()
+        {
+            if ( myClientToServerWaitingPackets == 0 )
+                throw new EndOfStreamException();
+
+            --myClientToServerWaitingPackets;
+
+            Monitor.Enter( myClientToServerStream );
+            myClientToServerStream.Position = myLastClientToServerReadPos;
+            return new BinaryReader( myClientToServerStream );
+        }
+
+        public static BinaryReader ReadServerToClientPacket()
+        {
+            if ( myServerToClientWaitingPackets == 0 )
+                throw new EndOfStreamException();
+
+            --myServerToClientWaitingPackets;
+
+            Monitor.Enter( myServerToClientStream );
+            myServerToClientStream.Position = myLastServerToClientReadPos;
+            return new BinaryReader( myServerToClientStream );
+        }
+
+        public static void EndReadingClientToServerPacket()
+        {
+            myLastClientToServerReadPos = myClientToServerStream.Position;
+            if ( myLastClientToServerReadPos == myLastClientToServerWritePos && myClientToServerStream.Length >= 2048 )
+            {
+                myClientToServerStream.Position = 0;
+                myLastClientToServerReadPos = 0;
+                myLastClientToServerWritePos = 0;
+            }
+            Monitor.Exit( myClientToServerStream );
+        }
+
+        public static void EndReadingServerToClientPacket()
+        {
+            myLastServerToClientReadPos = myServerToClientStream.Position;
+            if ( myLastServerToClientReadPos == myLastServerToClientWritePos && myServerToClientStream.Length >= 2048 )
+            {
+                myServerToClientStream.Position = 0;
+                myLastServerToClientReadPos = 0;
+                myLastServerToClientWritePos = 0;
+            }
+            Monitor.Exit( myServerToClientStream );
+        }
+
+        public static void Reset()
+        {
+            myClientToServerWaitingPackets = 0;
+            myClientToServerStream = new MemoryStream();
+            myLastClientToServerReadPos = 0;
+            myLastClientToServerWritePos = 0;
+
+            myServerToClientWaitingPackets = 0;
+            myServerToClientStream = new MemoryStream();
+            myLastServerToClientReadPos = 0;
+            myLastServerToClientWritePos = 0;
+        }
+    }
+}
---
+++ MarsMiner.Shared/Networking/Networking.cs
@@ -0,0 +1,242 @@
+﻿/**
+ * Copyright (c) 2012 James King [metapyziks@gmail.com]
+ *
+ * This file is part of MarsMiner.
+ * 
+ * MarsMiner is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * MarsMiner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MarsMiner. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace MarsMiner.Shared.Networking
+{
+    public delegate bool PacketHandlerDelegate( RemoteNetworkedObject sender,
+        PacketType type, Stream stream );
+
+    public class PacketType
+    {
+        public readonly UInt16 ID;
+        public readonly String Name;
+
+        public readonly PacketHandlerDelegate PacketHandler;
+
+        internal PacketType( UInt16 id, String name, PacketHandlerDelegate handler )
+        {
+            ID = id;
+            Name = name;
+
+            PacketHandler = handler;
+        }
+    }
+
+    public static class PacketManager
+    {
+        private static UInt16 stNextID = 0x0000;
+
+        private static Dictionary<String,PacketType> stTypeNames = new Dictionary<string, PacketType>();
+        private static PacketType[] stTypeIDs = new PacketType[ 0 ];
+
+        public static PacketType Register( String name, PacketHandlerDelegate handler )
+        {
+            if ( stTypeNames.ContainsKey( name ) )
+                throw new Exception( "Can not register new packet type:"
+                    + "packet type already registered with the name \"" + name + "\"" );
+
+            PacketType type = new PacketType( stNextID, name, handler );
+
+            stTypeIDs[ stNextID ] = type;
+            stTypeNames.Add( name, type );
+
+            ++stNextID;
+
+            return type;
+        }
+
+        public static PacketType GetType( String typeName )
+        {
+            return stTypeNames[ typeName ];
+        }
+
+        public static PacketType GetType( UInt16 typeID )
+        {
+            return stTypeIDs[ typeID ];
+        }
+
+        public static bool HandlePacket( RemoteNetworkedObject sender, Stream stream )
+        {
+            UInt16 id = BitConverter.ToUInt16( stream.ReadBytes( 2 ), 0 );
+
+            if ( stNextID <= id )
+                throw new Exception( "Unknown packet type recieved" );
+
+            PacketType type = stTypeIDs[ id ];
+            return type.PacketHandler( sender, type, stream );
+        }
+    }
+
+    public static class NetworkConstants
+    {
+        public const ushort ProtocolVersion = 0x0005;
+    }
+
+    public enum DisconnectReason : byte
+    {
+        ServerStopping = 0x00,
+        ServerFull = 0x01,
+        ProtocolVersionMismatch = 0x02,
+        Kicked = 0x03,
+        Timeout = 0x04,
+        ClientDisconnect = 0x05,
+        BadPassword = 0x06,
+        ResourceNotFound = 0x07
+    }
+
+    public class RemoteNetworkedObject
+    {
+        public static readonly PacketType AliveCheck;
+
+        static RemoteNetworkedObject()
+        {
+            AliveCheck = PacketManager.Register( "AliveCheck",
+                delegate( RemoteNetworkedObject sender,
+                    PacketType type, Stream stream )
+            {
+                switch ( stream.ReadByte() )
+                {
+                    case 0x00:
+                        sender.myTimingOut = false;
+                        return true;
+                    case 0xFF:
+                        sender.SendAliveCheck( false );
+                        return true;
+                    default:
+                        return false;
+                }
+            } );
+        }
+
+        private DateTime myLastReceivedTime;
+        private bool myTimingOut;
+
+        public double TimeOutDelay;
+        public double AliveCheckPeriod;
+
+        public double SecondsSinceLastPacket
+        {
+            get
+            {
+                return ( DateTime.Now - myLastReceivedTime ).TotalSeconds;
+            }
+        }
+
+        public bool TimingOut
+        {
+            get
+            {
+                if ( !myTimingOut && SecondsSinceLastPacket >= AliveCheckPeriod )
+                {
+                    SendAliveCheck( true );
+                    myTimingOut = true;
+                }
+
+                return myTimingOut;
+            }
+        }
+
+        public virtual IPAddress IPAddress
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public String SanitisedIPAddress
+        {
+            get
+            {
+                return IPAddress.ToString().Replace( '.', '_' );
+            }
+        }
+
+        protected virtual Stream SendStream
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        protected virtual Stream ReceiveStream
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public RemoteNetworkedObject()
+        {
+            myLastReceivedTime = DateTime.Now;
+            myTimingOut = false;
+
+            TimeOutDelay = 30.0;
+            AliveCheckPeriod = 2.0;
+        }
+
+        public virtual void CheckForPackets()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected bool ReadPacket()
+        {
+            OnReceivePacket();
+            myLastReceivedTime = DateTime.Now;
+            myTimingOut = false;
+            return PacketManager.HandlePacket( this, ReceiveStream );
+        }
+
+        protected virtual void OnReceivePacket()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Stream StartPacket( String typeName )
+        {
+            return StartPacket( PacketManager.GetType( typeName ) );
+        }
+
+        public virtual Stream StartPacket( PacketType type )
+        {
+            Stream str = SendStream;
+            str.Write( BitConverter.GetBytes( type.ID ), 0, 2 );
+            return str;
+        }
+
+        public virtual void SendPacket()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual bool PacketPending()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendAliveCheck( bool expectReply )
+        {
+            Stream str = StartPacket( AliveCheck );
+            str.WriteByte( (byte)( expectReply ? 0xFF : 0x00 ) );
+            SendPacket();
+        }
+    }
+}
